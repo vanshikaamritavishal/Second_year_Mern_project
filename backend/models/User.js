5723c307186c10040a58b50102383d7db382eab6
vanshikaamritavishal <vanshikaamritavishal@gmail.com>
2025-10-24 00:59:33 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   googleId: { type: String },
   name: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // ✅ add this
   age: Number,
   linkedin: String,
   college: String,
